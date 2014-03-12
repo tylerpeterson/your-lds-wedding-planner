54bed924b0922d7a64494087add504fd6cce055c
Tyler Peterson <petersontb@familysearch.org>
2014-03-12 17:21:21 -0600

    Heroku-ize the bare-bones site
    Parameterize the bound port.
    Add Procfile for Heroku.

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 // app.js
 
 var express = require('express'),
-    app = express();
+    app = express(),
+    port = Number(process.env.PORT || 5000);
 
 app.get('/', function (req, res) {
   res.send('Hello new bride!');
 });
 
-var server = app.listen(5000, function () {
+var server = app.listen(port, function () {
   console.log('listening on port %d', server.address().port);
 });

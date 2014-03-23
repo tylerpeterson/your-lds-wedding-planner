1479963c5650f7307f8ce31a88a2ffa280413424
Tyler Peterson <petersontb@familysearch.org>
2014-03-22 21:51:46 -0600

    Foundation for playing with flex box
    put in new layout that will use flexgrid
    depend on flex grid and bind the files to be served
    Will play with using flex grid in the flex layout.

--- app.js
+++ app.js
@@ -6,15 +6,26 @@ var express = require('express'),
     ejs = require('ejs'),
     Q = require('q'),
     fs = require('fs'),
-    renderFile = Q.denodeify(ejs.renderFile);
+    renderFile = Q.denodeify(ejs.renderFile),
+    flexgrid = require('flexgrid'),
+    ecstatic = require('ecstatic'),
+    hackedServe; // Stolen from tag-serve... If I keep it should make it reusable
+
+(function () {
+  var ecstaticMiddleware = ecstatic({root: '/'});
+  hackedServe = function (localFilename, res) {
+    ecstaticMiddleware({url: localFilename, method: 'GET'}, res);
+  };
+})();
 
 app.use(express.static(__dirname + '/public'));
 
-function renderView(path, options) {
-  var htmlDfd = Q.defer();
+function renderView(path, options, layout) {
+  var htmlDfd = Q.defer(),
+      layoutPath = 'ejs-layouts/' + (layout || 'standard') + '.ejs';
 
   renderFile(path, options).then(function (viewHtml) {
-    renderFile('ejs-layouts/standard.ejs', {body: viewHtml, locals: options.locals}).then(function (pageHtml) {
+    renderFile(layoutPath, {body: viewHtml, locals: options.locals}).then(function (pageHtml) {
       htmlDfd.resolve(pageHtml);
     }, htmlDfd.reject);
   }, htmlDfd.reject);
@@ -22,16 +33,36 @@ function renderView(path, options) {
   return htmlDfd.promise;
 }
 
-function renderContentPage(res, path, locals) {
+function renderContentPage(res, path, locals, layout) {
   options = {locals: locals};
   console.log('options', options);
-  renderView(path, options).then(function (pageHtml) {
+  renderView(path, options, layout).then(function (pageHtml) {
     res.send(pageHtml);
   }, function (err) {
     console.log('err rendering view', err);
     res.send(500, 'Sorry! Something broke.');
   });
 }
+
+function bindPathToFile(sitePath, filePath) {
+  app.get(sitePath, function (req, res) {
+    hackedServe(filePath, res);
+  });
+}
+
+// Bind all the flexgrid files into the server.
+// TODO move this into the flexgrid module? It is pretty icky.
+// Perhaps it would feel better as a middleware from the flexgrid module?
+
+for (var flexfile in flexgrid) {
+  if (flexgrid.hasOwnProperty(flexfile)) {
+    bindPathToFile('/flexgrid/' + flexfile, flexgrid[flexfile]);
+  }
+}
+
+app.get('/flex', function (req, res) {
+  renderContentPage(res, 'ejs-views/flex_home.ejs', {active: 'home'}, 'flex');
+});
 
 app.get('/about-ann', function (req, res) {
   renderContentPage(res, 'ejs-views/about_ann.ejs', {active: 'author'});
